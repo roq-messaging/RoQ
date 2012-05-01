d05e9773d4a5707f3a297297a1048bf6ccec6bdf
Sabri Skhiri <sabri.skhiri@euranova.eu>
2012-05-01 11:12:36 +0200

    Issue #56: Implementation of the subscriber logic at the controller.

--- roq-management/src/main/java/org/roqmessaging/management/server/MngtController.java
+++ roq-management/src/main/java/org/roqmessaging/management/server/MngtController.java
@@ -14,38 +14,54 @@
  */
 package org.roqmessaging.management.server;
 
+import java.util.HashMap;
+
 import org.apache.log4j.Logger;
+import org.roqmessaging.core.RoQConstant;
+import org.roqmessaging.core.interfaces.IStoppable;
+import org.roqmessaging.core.utils.RoQSerializationUtils;
 import org.roqmessaging.management.GlobalConfigurationManager;
 import org.zeromq.ZMQ;
 
 /**
  * Class MngtController
- * <p> Description: Controller that loads/receive  data from the {@linkplain GlobalConfigurationManager}
- * and refresh the stored data.
+ * <p>
+ * Description: Controller that loads/receive data from the
+ * {@linkplain GlobalConfigurationManager} and refresh the stored data.
  * 
  * @author sskhiri
  */
-public class MngtController implements Runnable{
+public class MngtController implements Runnable, IStoppable {
 	private Logger logger = Logger.getLogger(MngtController.class);
-	//ZMQ 
+	// ZMQ
 	private ZMQ.Context context = null;
 	private ZMQ.Socket mngtSubSocket = null;
-	//running
+	// running
 	private volatile boolean active = true;
-	
+	// utils for serialization
+	private RoQSerializationUtils serializationUtils = null;
+	//Management infra
+	private MngtServerStorage storage = null;
+
 	/**
 	 * Constructor.
-	 * @param globalConfigAddress the address on which the global config server runs.
+	 * 
+	 * @param globalConfigAddress
+	 *            the address on which the global config server runs.
 	 */
 	public MngtController(String globalConfigAddress) {
-		//Init ZMQ
+		// Init ZMQ
 		context = ZMQ.context(1);
 		mngtSubSocket = context.socket(ZMQ.SUB);
-		mngtSubSocket.connect("tcp://"+globalConfigAddress+":5000");
+		mngtSubSocket.connect("tcp://" + globalConfigAddress + ":5001");
+		// init variable
+		this.serializationUtils = new RoQSerializationUtils();
 	}
 
 	/**
-	 * Runs in a while loop and wait for updated information from the {@linkplain GlobalConfigurationManager}
+	 * Runs in a while loop and wait for updated information from the
+	 * {@linkplain GlobalConfigurationManager}
+	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	public void run() {
@@ -53,15 +69,65 @@ public class MngtController implements Runnable{
 		ZMQ.Poller poller = context.poller(3);
 		poller.register(mngtSubSocket);// 0
 
+		// Init variables
+		int infoCode = 0;
+
+		// Start the running loop
 		while (this.active && !Thread.currentThread().isInterrupted()) {
 			// Set the poll time out, it returns either when something arrive or
 			// when it time out
 			poller.poll(2000);
 			if (poller.pollin(0)) {
-				//An event arrives
+				// An event arrives start analysing code
+				String info[] = new String(mngtSubSocket.recv(0)).split(",");
+				// Check if exchanges are present: this happens when the queue
+				// is shutting down a client is asking for a
+				// connection
+				infoCode = Integer.parseInt(info[0]);
+				switch (infoCode) {
+				case RoQConstant.MNGT_UPDATE_CONFIG:
+					// Infocode, map(Q Name, host)
+					if (mngtSubSocket.hasReceiveMore()) {
+						HashMap<String, String> newConfig = this.serializationUtils.deserializeObject(mngtSubSocket
+								.recv(0));
+						updateConfiguration(newConfig);
+					} else {
+						// Problem expected map here
+						logger.error("Error, expected map of (Qname, host", new IllegalStateException(
+								"Expected a second message" + " in the envelope"));
+					}
+					break;
+
+				default:
+					break;
+				}
 			}
 		}
+		logger.info("Stopping " + this.getClass().getName() + " cleaning sockets");
+		this.mngtSubSocket.close();
+	}
 
+	/**
+	 * @param newConfig the updated configuration recieved each minute
+	 */
+	private void updateConfiguration(HashMap<String, String> newConfig) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see org.roqmessaging.core.interfaces.IStoppable#shutDown()
+	 */
+	public void shutDown() {
+		this.active = false;
+
+	}
+
+	/**
+	 * @see org.roqmessaging.core.interfaces.IStoppable#getName()
+	 */
+	public String getName() {
+		return this.getClass().getName() + " Server";
 	}
 
 }
