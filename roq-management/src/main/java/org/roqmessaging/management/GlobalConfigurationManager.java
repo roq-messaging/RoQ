eb561c38ae201a93bbf643ec10fcad024ea40d2d
Sabri Skhiri <sabri.skhiri@euranova.eu>
2012-05-31 18:40:41 +0200

    Increased the poll in the global configuration manager.

--- roq-management/src/main/java/org/roqmessaging/management/GlobalConfigurationManager.java
+++ roq-management/src/main/java/org/roqmessaging/management/GlobalConfigurationManager.java
@@ -116,7 +116,7 @@ public class GlobalConfigurationManager implements Runnable, IStoppable {
 		items.register(this.clientReqSocket);
 		//2. Start the main run of the monitor
 		while (this.running) {
-			items.poll(2000);
+			items.poll(10000);
 			if (items.pollin(0)){ //Comes from a client
 				byte[] encoded = clientReqSocket.recv(0);
 				String content = new String(encoded);
