ed6d59fc6dc0685fecc88e394d87ec1a0b2e389a
Sabri Skhiri <sabri.skhiri@euranova.eu>
2013-02-03 14:50:44 +0100

    Adding a description of a test load as a data object.

---
+++ roq-simulation/src/main/java/org/roqmessaging/loaders/TestLoaderDecription.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2013 EURANOVA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.roqmessaging.loaders;
+
+/**
+ * Class TestLoaderDecription
+ * <p> Description: Description of the test load. This is a data object that will be use by the Load controller
+ * to launch the tests.
+ *
+ * <br>  The Spawn rate x: every x second we start a producer. This is similar as a warm-up period. Default =1 prod every second
+ * <br>      Max producers y: the maximum number of producers to spawn. Default =1
+ * <br>     Rate (msg/s) r: the number of messages per minute to be sent by each spawned producer. Default =1msg/s
+ * <br>     Duration (min) d: the test duration.  Default =1 min
+ * <br>     Payload (kb) p: the message content size, simulated by an array of px1000 size of byte. Default =1 kb
+ * <br>     Number of subscribers s: the maximum number of producers to spawn . Default =1
+ * <br>     Delay (s) de: the waiting time before starting the test.  Default = 5s
+
+ * 
+ * @author sskhiri
+ */
+public class TestLoaderDecription {
+	private int spawnRate =1;
+	private int maxProd = 1;
+	private int rate =1;
+	private float duration = 1.0f;
+	private int payload = 1;
+	private int maxSub = 1;
+	private int delay = 5;
+	/**
+	 * @return the spawnRate
+	 */
+	public int getSpawnRate() {
+		return spawnRate;
+	}
+	/**
+	 * @param spawnRate the spawnRate to set
+	 */
+	public void setSpawnRate(int spawnRate) {
+		this.spawnRate = spawnRate;
+	}
+	/**
+	 * @return the maxProd
+	 */
+	public int getMaxProd() {
+		return maxProd;
+	}
+	/**
+	 * @param maxProd the maxProd to set
+	 */
+	public void setMaxProd(int maxProd) {
+		this.maxProd = maxProd;
+	}
+	/**
+	 * @return the rate
+	 */
+	public int getRate() {
+		return rate;
+	}
+	/**
+	 * @param rate the rate to set
+	 */
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	/**
+	 * @return the duration
+	 */
+	public float getDuration() {
+		return duration;
+	}
+	/**
+	 * @param duration the duration to set
+	 */
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+	/**
+	 * @return the payload
+	 */
+	public int getPayload() {
+		return payload;
+	}
+	/**
+	 * @param payload the payload to set
+	 */
+	public void setPayload(int payload) {
+		this.payload = payload;
+	}
+	/**
+	 * @return the maxSub
+	 */
+	public int getMaxSub() {
+		return maxSub;
+	}
+	/**
+	 * @param maxSub the maxSub to set
+	 */
+	public void setMaxSub(int maxSub) {
+		this.maxSub = maxSub;
+	}
+	/**
+	 * @return the delay
+	 */
+	public int getDelay() {
+		return delay;
+	}
+	/**
+	 * @param delay the delay to set
+	 */
+	public void setDelay(int delay) {
+		this.delay = delay;
+	}
+
+}
