1338bc61cf66ff13a3f26ad61f6618f53710f957
sskhiri <sabri.skhiri@euranova.eu>
2014-02-09 12:22:43 +0100

    Update the scripts to run new updated versions.

--- roq-core/src/main/resources/scripts/pkg-scripts/startGCM.sh
+++ roq-core/src/main/resources/scripts/pkg-scripts/startGCM.sh
@@ -3,11 +3,13 @@ echo "Starting Global configuration process"
 #Get the local directory in order to set the full path to the log4j config file
 ROQ_BIN=$(dirname "$(readlink -f "$(type -P $0 || echo $0)")")
 ROQ_HOME=$(dirname $ROQ_BIN)
+VERSION="1.1"
 echo "ROQ BIN Home= $ROQ_HOME"
+echo "Launching RoQ $VERSION ..."
 
 if [ -n "$1" ]; then
 	echo "Input starting script $@"
-	 java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp ../lib/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar org.roqmessaging.management.launcher.GlobalConfigurationLauncher $@
+	 java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp $ROQ_HOME/lib/roq-management-$VERSION-jar-with-dependencies.jar org.roqmessaging.management.launcher.GlobalConfigurationLauncher $@
 else
-	 java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp ../lib/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar org.roqmessaging.management.launcher.GlobalConfigurationLauncher ../config/GCM.properties
+	 java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp $ROQ_HOME/lib/roq-management-$VERSION-jar-with-dependencies.jar org.roqmessaging.management.launcher.GlobalConfigurationLauncher $ROQ_HOME/config/GCM.properties
 fi
--- roq-core/src/main/resources/scripts/pkg-scripts/stopRoQ.sh
+++ roq-core/src/main/resources/scripts/pkg-scripts/stopRoQ.sh
@@ -3,12 +3,14 @@ echo "Starting Host manager process"
 #Get the local directory in order to set the full path to the log4j config file
 ROQ_BIN=$(dirname "$(readlink -f "$(type -P $0 || echo $0)")")
 ROQ_HOME=$(dirname $ROQ_BIN)
+VERSION="1.1"
 echo "ROQ BIN Home= $ROQ_HOME"
+echo "Launching RoQ $VERSION ..."
 
 # INPUT: either <CGM> for stopping the GCM or <HCM> for stopping the HCM
 if [ -n "$1" ]; then
 	echo "Input starting script $@"
-	java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp ../lib/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar org.roqmessaging.management.launcher.RoQClusterStopper $@
+	java -Djava.library.path=/usr/local/lib -Dlog4j.configuration="file:$ROQ_HOME/config/log4j.properties" -cp $ROQ_HOME/lib/roq-management-$VERSION-SNAPSHOT-jar-with-dependencies.jar org.roqmessaging.management.launcher.RoQClusterStopper $@
 else
 	echo "The usage is ./stopRoQ GCM or ./stopRoQ HCM"
 fi
