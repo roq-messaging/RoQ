6df392fb588456267aa296ce8371a31e0664619b
bvanmelle <vanmelle.benjamin@gmail.com>
2015-02-26 16:37:18 +0100

    automation script modifications: automatic login to containers

--- roq-deployment/demonstration/demo-start-publisher.sh
+++ roq-deployment/demonstration/demo-start-publisher.sh
@@ -6,7 +6,7 @@ GCMIP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ROQGCM);
 
 # Open an ssh connection to the publisher container 
 # and run the publisher's daemon inside
-ssh -o StrictHostKeyChecking=no root@$PUBIP \
+sshpass -pscreencast ssh -o StrictHostKeyChecking=no root@$PUBIP \
 		java -Djava.library.path=/usr/local/lib -cp \
 		/lib/ROQ/roq-demonstration/target/roq-demonstration-1.0-SNAPSHOT-jar-with-dependencies.jar \
 		org.roq.demonstration.RoQDemonstrationPublisherLauncher $GCMIP testQ
--- roq-deployment/demonstration/demo-start-subscriber.sh
+++ roq-deployment/demonstration/demo-start-subscriber.sh
@@ -4,7 +4,7 @@
 ./deploy-roq-containers.sh
 
 # Get Containers IP addresses
-IP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ROQSUB);
+IPSUB=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ROQSUB);
 GCMIP=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ROQGCM);
 
 # Wait for SSH server availability
@@ -12,7 +12,7 @@ sleep 3
 
 # Open an ssh connection to the publisher container 
 # and run the publisher's daemon inside
-ssh -o StrictHostKeyChecking=no root@$IP \
+sshpass -pscreencast ssh -o StrictHostKeyChecking=no root@$IPSUB \
 		java -Djava.library.path=/usr/local/lib -cp \
 		/lib/ROQ/roq-demonstration/target/roq-demonstration-1.0-SNAPSHOT-jar-with-dependencies.jar \
 		org.roq.demonstration.RoQDemonstrationSubscriberLauncher $GCMIP testQ
