4190d321a50bcdcfeee6f8d35177f9cedfcd6dd1
bvanmelle <vanmelle.benjamin@gmail.com>
2015-03-03 09:52:15 +0100

    reorganise development scripts

--- roq-deployment/development/restart-components.sh
+++ roq-deployment/development/restart-components.sh
@@ -10,8 +10,9 @@ sudo docker stop ROQZK
 sudo docker rm ROQZK
 
 # Create inventory for Ansible
-echo [demo] > ansible/testInventory
-echo localhost ansible_connection=local >> ansible/testInventory
+echo [localSystem] > ../ansible/testInventory
+echo localhost ansible_connection=local >> ../ansible/testInventory
+echo [demo] >> ../ansible/testInventory
 
 # Build the zookeeper docker image
 sudo docker build -t debian:zookeeper ../docker/zookeeper
@@ -21,25 +22,25 @@ echo Starting zookeeper container
 sudo docker run --name ROQZK -d debian:zookeeper
 
 # Update inventory
-echo GCM.roq.org ansible_ssh_host=127.0.0.1 >> ansible/testInventory
+echo GCM.roq.org ansible_ssh_host=127.0.0.1 >> ../ansible/testInventory
 echo ZK.roq.org ansible_ssh_host=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ROQZK) \
-		ansible_ssh_user=root >> ansible/testInventory
+		ansible_ssh_user=root >> ../ansible/testInventory
 
 # Launch the ansible playbook that update Zk address in the GCM.properties
-ansible-playbook -i ansible/testInventory ansible/gimmeROQ.yml --tags "update-gcm,update-hcm"
+ansible-playbook -i ../ansible/testInventory ../ansible/gimmeROQ.yml --tags "update-gcm,update-hcm"
 
-mvn install -DskipTests -f ../pom.xml
+mvn install -DskipTests -f ../../pom.xml
 
-echo starting GCM container
+echo starting GCM
 # Start GCM
 java -Djava.library.path=/usr/local/lib \
-		-cp ../roq-management/target/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar \
+		-cp ../../roq-management/target/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar \
 		org.roqmessaging.management.launcher.GlobalConfigurationLauncher \
 		~/.roq/GCM.properties &
 
-echo starting HCM container
+echo starting HCM
 # Start HCM
 java -Djava.library.path=/usr/local/lib \
-		-cp ../roq-management/target/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar \
+		-cp ../../roq-management/target/roq-management-1.0-SNAPSHOT-jar-with-dependencies.jar \
 		org.roqmessaging.management.launcher.HostConfigManagerLauncher \
 		~/.roq/HCM.properties &
